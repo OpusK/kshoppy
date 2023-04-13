e99598f5648767470e67079375cd09edcb849022
Kei <opusk44@gmail.com>
2023-04-13 21:39:11 +0900

    add adminUser check function

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -6,6 +6,7 @@ import {
   signOut,
   onAuthStateChanged,
 } from 'firebase/auth';
+import { getDatabase, ref, get } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -14,11 +15,12 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 // https://firebase.google.com/docs/auth/web/google-signin?hl=en&authuser=0
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
+const database = getDatabase(app);
 
 export function login() {
   signInWithPopup(auth, provider).catch(console.error);
@@ -31,7 +33,21 @@ export function logout() {
 
 // https://firebase.google.com/docs/auth/web/manage-users#get_the_currently_signed-in_user
 export function onUserStateChange(callback) {
-  onAuthStateChanged(auth, (user) => {
-    callback(user);
+  onAuthStateChanged(auth, async (user) => {
+    const updatedUser = user ? await adminUser(user) : null;
+    callback(updatedUser);
   });
+}
+
+// https://firebase.google.com/docs/database/web/read-and-write?hl=en&authuser=0
+async function adminUser(user) {
+  return get(ref(database, 'admins')) //
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        const admins = snapshot.val();
+        const isAdmin = admins.includes(user.uid);
+        return { ...user, isAdmin };
+      }
+      return user;
+    });
 }
